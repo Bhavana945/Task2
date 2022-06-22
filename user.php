9f24ae6cd4ed8e943fe16a8e69ab2a5170ca55ce
Bhavana945 <108012782+Bhavana945@users.noreply.github.com>
2022-06-22 20:46:30 +0530

    Add files via upload

---
+++ user.php
@@ -0,0 +1,9 @@
+<h1>user page</h1>
+<p>Hiii!.. This is Bhavana.. and Lalithya</p>
+<p>Discussing about WEB 3.O</p>
+<p><h2>Web1, Web2, and Web3: history, explanation, and definition</h2>
+The first commercial Web1 applications emerged in the early 1990s. At that time, "the Internet" consisted of static web pages. It was a "read-only" Internet, in that users could only consume information. Platforms like Facebook or Netflix didn't exist back then; the stars were websites like Britannica Online or Yahoo. IP, HTTP, URI, and HTML provided the technological infrastructure.
+
+In 2000, the dot-com bubble burst, and many Internet companies disappeared from the market. Despite this setback, the bright minds of the tech world focused on the further development of innovations such as JavaScript, HTML5, and CSS3. The result was the gradual rise of Web2.
+
+While Web1 mainly enabled the transmission of information, Web2 focuses on interaction: Users make their data available to service providers such as social media, e-commerce, or payments providers, enabling these apps to function. In other words: Web2 users are no longer just consumers; they are also the product.</p>
